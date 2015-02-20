160a709b508ae39f97bfce44d4888b7296a78673
Arinde Eniola <arindeeniola@yahoo.com>
2015-02-20 16:59:06 +0100

    completed bidec

--- bidec.py
+++ bidec.py
@@ -2,7 +2,22 @@ if __name__ == '__main__':
 	print "Enter B to convert decimal to binary \n"\
 	      "and D to convert binary to decimal"
 
+def converter(num, letter):
+	if letter == "B":
+		return bin(int(num))[2:]
+	elif letter == "D":
+		con = "%s%s" %("0b", num)
+		return int(con, base=2)
+
+
 while True: 
 	input = raw_input("Enter a letter: ")
-	if(type)
+	if input.isalpha():
+		try:
+			inp = raw_input("Enter the number to convert: ")
+			print converter(inp, input)
+		except ValueError:
+			print "enter a valid decimal or binary Number"		
+	else:
+		print "Please input a valid letter"
 		
